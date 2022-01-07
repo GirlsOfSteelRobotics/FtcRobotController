7cbd652baec070aaccc15f3850dec1d17d0929e6
Girls of Steel FTC 9821 <2601974+geekygirlsarah@users.noreply.github.com>
2022-01-06 20:32:35 -0500

    Revised Hopper motor directions and added claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpHopper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpHopper.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
 
@@ -13,6 +14,7 @@ public class TeleOpHopper extends OpMode {
     private DcMotor TopRight = null;
     private DcMotor BottomLeft = null;
     private DcMotor BottomRight = null;
+    private DcMotor ClawMotor = null;
 
 
     @Override
@@ -21,16 +23,19 @@ public class TeleOpHopper extends OpMode {
         telemetry.update();
 
         TopLeft  = hardwareMap.dcMotor.get("TopLeft");
-//        TopLeft.setDirection(DcMotor.Direction.FORWARD);
+       TopLeft.setDirection(DcMotor.Direction.FORWARD);
 
         TopRight  = hardwareMap.dcMotor.get("TopRight");
-//        TopRight.setDirection(DcMotor.Direction.FORWARD);
+        TopRight.setDirection(DcMotor.Direction.REVERSE);
 
         BottomLeft  = hardwareMap.dcMotor.get("BottomLeft");
-//        BottomLeft.setDirection(DcMotor.Direction.FORWARD);
+       BottomLeft.setDirection(DcMotor.Direction.FORWARD);
 
         BottomRight  = hardwareMap.dcMotor.get("BottomRight");
-//        BottomRight.setDirection(DcMotor.Direction.FORWARD);
+        BottomRight.setDirection(DcMotor.Direction.REVERSE);
+
+        ClawMotor = hardwareMap.dcMotor.get("ClawMotor");
+        ClawMotor.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
@@ -38,17 +43,25 @@ public class TeleOpHopper extends OpMode {
         telemetry.addLine("Loop");
         telemetry.update();
 
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        double leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        double rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+     double drive = -gamepad1.left_stick_y;
+     double turn  =  gamepad1.right_stick_x;
+     double leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+     double rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-        TopLeft.setPower(leftPower);
-        TopRight.setPower(rightPower);
-        BottomLeft.setPower(leftPower);
-        BottomRight.setPower(rightPower);
+       TopLeft.setPower(leftPower);
+       TopRight.setPower(rightPower);
+       BottomLeft.setPower(leftPower);
+       BottomRight.setPower(rightPower);
 
-
+        if (gamepad1.y){
+            ClawMotor.setPower(0.3);
+        }
+        else if (gamepad1.a){
+            ClawMotor.setPower(-0.3);
+        }
+        else {
+            ClawMotor.setPower(0.0);
+        }
 //        double topLeftPower   = Range.clip(drive+turn,-1.0,1.0);
 //        double topRightPower   = Range.clip(drive-turn,-1.0,1.0);
 //        double bottomLeftPower   = Range.clip(-drive-turn,-1.0,1.0);

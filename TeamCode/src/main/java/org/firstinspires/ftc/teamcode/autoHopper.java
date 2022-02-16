f9bdc3e47b61020e7390a708b809a40b3d52152e
FrowntownPitt <ajf93@pitt.edu>
2022-02-15 20:18:18 -0500

    Autonomous code for Hopper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoHopper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoHopper.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
@@ -22,32 +23,27 @@ public class autoHopper extends LinearOpMode {
     private DcMotor ClawMotor = null;
     private Servo ClawServo = null;
 
-    HardwareMap hwMap =  null;
+    HardwareMap hwMap = null;
     HardwarePushbot robot = new HardwarePushbot();   // Use a Pushbot's hardware
     private ElapsedTime runtime = new ElapsedTime();
-
 
     static final double FORWARD_SPEED = 0.6;
     static final double TURN_SPEED = 0.5;
 
-    public void init(HardwareMap ahwMap){
+    public void init(HardwareMap ahwMap) {
         hwMap = ahwMap;
         // Define and Initialize Motors
-        TopLeft  = hwMap.get(DcMotor.class, "Top_Left");
+        TopLeft = hwMap.get(DcMotor.class, "Top_Left");
         TopRight = hwMap.get(DcMotor.class, "Top_Right");
         BottomLeft = hwMap.get(DcMotor.class, "Bottom_Left");
         BottomRight = hwMap.get(DcMotor.class, "Bottom_Right");
         ClawMotor = hwMap.get(DcMotor.class, "Claw_Motor");
-        leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-
-
+        //leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        //rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
     }
 
-
     @Override
-    public void runOpMode () {
-
+    public void runOpMode() {
         /*
          * Initialize the drive system variables.
          * The init() method of the hardware class does all the work here
@@ -60,7 +56,18 @@ public class autoHopper extends LinearOpMode {
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        // motion starts in this one or in a new public void? -gray
-    }
 
+        TopLeft.setPower(0.5);
+        TopRight.setPower(0.5);
+        BottomLeft.setPower(0.5);
+        BottomRight.setPower(0.5);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+            telemetry.addData("Path", runtime.seconds());
+            telemetry.update();
+
+
+        }
+
+    }
 }
